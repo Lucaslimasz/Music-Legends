f2b335cf778696863985bf6a32a558fb8439f2b5
Lucaslimasz <id.lucas@outlook.com.br>
2022-04-02 09:32:34 -0300

    style global and header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,18 @@
+import styled from './styles.module.scss';
+
+export const Header = () => {
+  return (
+    <header className={styled.header}>
+      <ul>
+        <li>Inicio</li>
+        <li>Buscar</li>
+        <li>Suas Bibliotecas</li>
+      </ul>
+      <h1>Music Legends</h1>
+      <div>
+        <p>Iuri Silva</p>
+        <img src="https://github.com/lucaslimasz.png" alt="" />
+      </div>
+    </header>
+  );
+};
