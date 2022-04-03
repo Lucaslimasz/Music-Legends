27c1ded8ca43acbf145a04ccad8dade02af453d5
Lucaslimasz <id.lucas@outlook.com.br>
2022-04-03 00:58:11 -0300

    Slider

--- src/components/Player/index.jsx
+++ src/components/Player/index.jsx
@@ -1,3 +1,6 @@
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
+
 import styled from "./styles.module.scss";
 
 import Heart from "../../assets/icons/heart.svg";
@@ -9,43 +12,58 @@ import Shuffle from "../../assets/icons/repeat.svg";
 import Volume from "../../assets/icons/volume.svg";
 import Minimize from "../../assets/icons/minimize.svg";
 
+import Server from "../../utils/server";
+
 export const Player = () => {
+  const option = Server[0];
   return (
-    <div className={styled.player}>
-      <div className={styled.information}>
-        <img src={Heart} alt="Favoritar" />
-        <div>
-          <strong>Warrior</strong>
-          <span>Riot games ft. Imagine Dragons</span>
+    <>
+      <div className={styled.Slider}>
+        <Slider
+          trackStyle={{ backgroundColor: "#4ac08f" }}
+          railStyle={{ backgroundColor: "#2c123f" }}
+          handleStyle={{ borderBlockColor: "#2EF4CC", borderWidth: 4 }}
+        />
+      </div>
+      <div className={styled.player}>
+        <div className={styled.information}>
+          <img src={Heart} alt="Favoritar" />
+          <div>
+            <strong>{option.title}</strong>
+            <span>{option.name}</span>
+          </div>
+        </div>
+
+        <div className={styled.actions}>
+          <button>
+            <img src={Shuffle} alt="" />
+          </button>
+          <button>
+            <img src={Previous} alt="" />
+          </button>
+          <button className={styled.buttonPlay}>
+            <img src={Play} alt="" />
+          </button>
+          <button>
+            <img src={Next} alt="" />
+          </button>
+          <button>
+            <img src={Repeat} alt="" />
+          </button>
+        </div>
+
+        <audio controls src={option.url} />
+        {console.log(option.url)}
+
+        <div className={styled.buttonEffect}>
+          <button>
+            <img src={Volume} alt="" />
+          </button>
+          <button>
+            <img src={Minimize} alt="" />
+          </button>
         </div>
       </div>
-
-      <div className={styled.actions}>
-        <button>
-          <img src={Shuffle} alt="" />
-        </button>
-        <button>
-          <img src={Previous} alt="" />
-        </button>
-        <button className={styled.buttonPlay}>
-          <img src={Play} alt="" />
-        </button>
-        <button>
-          <img src={Next} alt="" />
-        </button>
-        <button>
-          <img src={Repeat} alt="" />
-        </button>
-      </div>
-
-      <div className={styled.buttonEffect}>
-        <button>
-          <img src={Volume} alt="" />
-        </button>
-        <button>
-          <img src={Minimize} alt="" />
-        </button>
-      </div>
-    </div>
+    </>
   );
 };
---
+++ src/utils/server.js
@@ -0,0 +1,10 @@
+const Server = [
+  {
+    id: Math.random(),
+    name: "Lunar Revel 2022 Theme",
+    title: "Warrior",
+    url: "../assets/music/League-of-Legends-Lunar-Revel-2022-Theme.mp3",
+  },
+];
+
+export default Server;
